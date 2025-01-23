5d102d192725a2bef491619a015f3457cc5d79e0
Nicholas Jones <nicholas.jones@veovo.com>
2025-01-23 19:47:57 +0100

    Extend support for classes when initializing event listeners.

--- services/event-service.js
+++ services/event-service.js
@@ -3,11 +3,35 @@ class EventListenerService {
     this.events = [];
   }
 
+  _addEventListenerOnId(eventSubject) {
+    const id = eventSubject.id;
+    const eventType = eventSubject.eventType;
+    const action = eventSubject.action;
+
+    document.getElementById(id).addEventListener(eventType, action);
+  }
+
+  _addEventListenersOnClasses(eventSubject) {
+    const className = eventSubject.class;
+    const eventType = eventSubject.eventType;
+    const action = eventSubject.action;
+    const elements = document.getElementsByClassName(className);
+
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].addEventListener(eventType, action);
+    }
+  }
+
   initializeEventListeners() {
     this.events.forEach((eventSubject) => {
-      document
-        .getElementById(eventSubject.id)
-        .addEventListener(eventSubject.eventType, eventSubject.action);
+      const eventUsesId = Object.keys(eventSubject).indexOf("id") !== -1;
+      const eventUsesClass = Object.keys(eventSubject).indexOf("class") !== -1;
+
+      if (eventUsesId) {
+        this._addEventListenerOnId(eventSubject);
+      } else if (eventUsesClass) {
+        this._addEventListenersOnClasses(eventSubject);
+      }
     });
   }
 }
