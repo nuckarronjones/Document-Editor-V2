8af8319ac164476ebaa16a185ba0368404dca528
Nicholas Jones <njonesgrad@gmail.com>
2025-02-27 16:18:24 +0100

    Separate Document Data Options
    Separate const lists of data selection options into a separate data file. Reduce bloat in existing components. Examples like font, line spacing, etc.

---
+++ frontend/data/documentData.js
@@ -0,0 +1,84 @@
+export const SHAPES = [
+  {
+    square: "https://img.icons8.com/windows/96/000000/unchecked-checkbox.png",
+  },
+  {
+    circle: "https://img.icons8.com/ios-glyphs/100/000000/circled.png",
+  },
+  {
+    octagon: "https://img.icons8.com/material-rounded/96/000000/octagon.png",
+  },
+  {
+    star: "https://img.icons8.com/material-outlined/96/000000/star--v2.png",
+  },
+  {
+    triangle:
+      "https://img.icons8.com/material-outlined/96/000000/triangle-stroked.png",
+  },
+  {
+    heart: "https://img.icons8.com/material-outlined/96/000000/like--v1.png",
+  },
+  {
+    link: "https://img.icons8.com/material-outlined/96/000000/link--v1.png",
+  },
+  {
+    person:
+      "https://img.icons8.com/pastel-glyph/100/000000/person-male--v1.png",
+  },
+  {
+    wheelchair:
+      "https://img.icons8.com/ios-glyphs/100/000000/sporty-wheelchair-user.png",
+  },
+  {
+    cloud: "https://img.icons8.com/material-outlined/96/000000/cloud--v1.png",
+  },
+  {
+    sun: "https://img.icons8.com/material-outlined/96/000000/sun--v1.png",
+  },
+  {
+    moon: "https://img.icons8.com/ios/100/000000/crescent-moon.png",
+  },
+  {
+    group:
+      "https://img.icons8.com/ios-glyphs/100/000000/group-foreground-selected.png",
+  },
+  {
+    hand: "https://img.icons8.com/ios/96/000000/hand.png",
+  },
+  {
+    cursor: "https://img.icons8.com/ios/96/000000/cursor--v1.png",
+  },
+];
+
+export const FONTLIST = [
+    "Arial",
+    "Arial Black",
+    "Verdana",
+    "Tahoma",
+    "Trebuchet MS",
+    "Impact",
+    "Times New Roman",
+    "Courier",
+    "Lucida Console",
+    "Luminari",
+    "Comic Sans MS",
+  ].sort();
+
+export const COLORS = [
+    ["red", "#FF0000"],
+    ["orange", "#FF7F00"],
+    ["yellow", "#FFFF00"],
+    ["green", "#00FF00"],
+    ["blue", "#0000FF"],
+    ["indigo", "#4B0082"],
+    ["violet", "#9400D3"],
+    ["black", "#000000"],
+    ["white", "#FFFFFF"],
+  ];
+
+export const LINE_SPACINGS = {
+    Single: "1",
+    1.15: "1.15",
+    1.5: "1.5",
+    Double: "2",
+  };
