6901a8f6ab481a029ac24547c7b39e2160e71af7
Nicholas Jones <njonesgrad@gmail.com>
2025-02-27 16:15:15 +0100

    Set Up POST Request Handling + Json Middleware

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,18 @@ const port = 3000
 const frontendPath = path.join(__dirname, '../frontend');
 
 app.use(express.static(frontendPath));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     es.sendFile(path.join(frontendPath, 'index.html'));
 })
+
+app.post("/", (req, res) => {
+  const data = req.body;
+  console.log("Received data:", data);
+
+  res.json({ message: "Data received successfully", received: data });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
