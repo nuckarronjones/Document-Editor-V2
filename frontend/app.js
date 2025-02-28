0520047fb06444e58d31757e57d9065efbd1165a
Nicholas Jones <njonesgrad@gmail.com>
2025-02-28 18:47:27 +0100

    Add Simple Routing and Rendering Functionality
    Add simple routing between home page and document editor. Right now this works, but I want to clean this up a bit in the near future, and see how can I can streamline this.  window.history.pushState was a great find to get this working.

--- frontend/app.js
+++ frontend/app.js
@@ -3,32 +3,57 @@ import { DocumentComponent } from "./pages/document-editor-page/documentComponen
 import { eventListenerService } from "./services/eventService.js";
 import { ShapesModal } from "./pages/document-editor-page/shapesModalComponent/shapesModal.js";
 import { UserDocumentsComponent } from "./pages/user-documents-page/userDocumentsComponent/userDocumentsComponent.js";
+import { routingService } from "./services/routingService.js";
 
 class App {
   constructor() {
-    this.eventListenerService = eventListenerService;
     this.documentComponent = new DocumentComponent();
     this.navbarComponent = new NavToolbarComponent();
     this.userDocumentsComponent = new UserDocumentsComponent();
     this.shapesModal = new ShapesModal();
+
+    this.eventListenerService = eventListenerService;
+    this.routingService = routingService;
+
+    //Bind this context, so we dont lose it when passed in as a callback
+    this.onRefreshCallback = this.onRefreshCallback.bind(this);
+    //Detect refresh, and use our callback to re-initialize page contents/listeners
+    this.routingService.detectRefresh(this.onRefreshCallback);
+
+    //Boostrap app as soon as its constructed
+    this.bootstrap();
   }
 
-  bootstrap = () => {
+  bootstrap() {
     const appElement = document.getElementById("app");
 
     const html = `
-        ${this.navbarComponent.render()}
-        ${this.documentComponent.render()}
+      ${this.userDocumentsComponent.render()}
     `;
 
-    // const html= `
-    //   ${this.userDocumentsComponent.render()}
-    // `;
-
+    //Initialize page content first, then listeners. Must be in this order to attach listeners to html
     appElement.innerHTML = html;
     this.eventListenerService.initializeEventListeners();
-  };
+  }
+
+  onRefreshCallback(url) {
+    const appElement = document.getElementById("app");
+
+    this.eventListenerService.emptyEventListeners();
+
+    if (url.includes("/editor")) {
+      appElement.innerHTML = `
+         ${this.navbarComponent.render()}
+         ${this.documentComponent.render()}
+     `;
+    } else {
+      appElement.innerHTML = `
+      ${this.userDocumentsComponent.render()}
+    `;
+    }
+
+    this.eventListenerService.initializeEventListeners();
+  }
 }
 
-const app = new App();
-app.bootstrap();
+new App();
--- frontend/pages/document-editor-page/navToolbarComponent/documentSettingsComponent/documentSettings.js
+++ frontend/pages/document-editor-page/navToolbarComponent/documentSettingsComponent/documentSettings.js
@@ -1,12 +1,14 @@
 import { eventListenerService } from "../../../../services/eventService.js";
 import { toolbarService } from "../../../../services/toolbarService.js";
 import { modalService } from "../../../../services/modalService.js";
+import { routingService } from "../../../../services/routingService.js";
 
 export class DocumentSettingsComponent {
   constructor() {
     this.eventListenerService = eventListenerService;
     this.toolbarService = toolbarService;
     this.modalService = modalService;
+    this.routingService = routingService;
   }
 
   events = [
@@ -40,6 +42,11 @@ export class DocumentSettingsComponent {
       eventType: "click",
       action: () => this.modalService.setModalSubject("ShapesModal"),
     },
+    {
+      id: "home-button",
+      eventType: "click",
+      action: () => this.routingService.setRoute("/"),
+    },
   ];
 
   renderDropdown(targetId) {
@@ -60,8 +67,8 @@ export class DocumentSettingsComponent {
     return `
            <div id="name_Toolbar" class="toolbar">
                 <ul class="text_Settings_Tools">
-                    <li>
-                    <i id="logo" class="bi bi-book"></i>
+                    <li 
+                      <button id="home-button"><i id="logo" class="bi bi-book"></i></button>
                     </li>
                     <li>
                     <input id="documentName" type="" name="" value="Document Title" />
