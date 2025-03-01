0d4bd250c9f98ff5cf21a277aad75ec2d3933455
Nicholas Jones <njonesgrad@gmail.com>
2025-03-01 11:04:10 +0100

    Move SaveDocument Function
    Move save document function from toolbarService to DocumentServiceApi

--- frontend/services/toolbarService.js
+++ frontend/services/toolbarService.js
@@ -98,22 +98,6 @@ class ToolbarService {
     windowedDocument.print();
   }
 
-  saveDocument(){
-    fetch('/', {
-      method: 'POST', 
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-          key1: 'value1',
-          key2: 'value2'
-      })
-  })
-  .then(response => response.json())
-  .then(data => console.log('Success:', data))
-  .catch(error => console.error('Error:', error));
-  }
-
   renderDropdown(targetId) {
     const clickedElement = targetId;
     const currentActiveDropdown = this.activeDropdownElementId;
