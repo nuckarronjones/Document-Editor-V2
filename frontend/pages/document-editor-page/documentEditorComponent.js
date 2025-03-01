186486e1dae8382e4d0581349406a5994ad9095d
Nicholas Jones <njonesgrad@gmail.com>
2025-03-01 09:34:29 +0100

    Update Paths

--- frontend/pages/document-editor-page/documentEditorComponent.js
+++ frontend/pages/document-editor-page/documentEditorComponent.js
@@ -1,5 +1,5 @@
-import { DocumentComponent } from "../../document-editor-page/documentComponent/document.js";
-import { NavToolbarComponent } from "../../document-editor-page/navToolbarComponent/navToolbar.js";
+import { DocumentComponent } from "./documentComponent/document.js";
+import { NavToolbarComponent } from "./navToolbarComponent/navToolbar.js";
 export class DocumentEditorComponent {
   constructor() {
     this.documentComponent = new DocumentComponent();
