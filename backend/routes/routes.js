be3d45b8ebf55e6310b5a716b29d1b30ddd067bf
Nicholas Jones <njonesgrad@gmail.com>
2025-03-21 14:18:01 -0500

    Post Route to Retrieve All User Documents

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -92,4 +92,35 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.post("/allUserDocuments", async (req, res) => {
+  const data = req.body;
+  try {
+    const decoded = jwt.verify(data.token, process.env.JWT_SECRET);
+    const username = req.body.username;
+
+    if (decoded) {
+      try {
+        const user = await User.findOne({ username: username }).populate(
+          "documents"
+        );
+        const userDocuments = user.documents;
+
+        if (userDocuments) {
+          res
+            .status(200)
+            .json({
+              documents: user.documents,
+              message: "Retrieved user documents.",
+            });
+        } else {
+        }
+      } catch (error) {
+        res.status(400).json({ message: "Cannot locate user documents." });
+      }
+    }
+  } catch {
+    res.status(403).json({ error: "Forbidden: Invalid token" });
+  }
+});
+
 module.exports = router;
