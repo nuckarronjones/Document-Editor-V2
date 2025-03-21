83586a504bdd5419efa91318369af64550f59e4a
Nicholas Jones <njonesgrad@gmail.com>
2025-03-21 13:39:20 -0500

    User Documents Page
    Format user documents page + api method to retrieve all user documents

--- frontend/pages/userDocumentsPage/userDocumentsPageComponent.js
+++ frontend/pages/userDocumentsPage/userDocumentsPageComponent.js
@@ -3,7 +3,7 @@ import { routingService } from "../../services/routingService.js";
 import { documentPreferencesService } from "../../services/documentPreferencesService.js";
 import { generateDocumentId } from "../../funcions/generateDocumentId.js";
 
-export class UserDocumentsPageComponent{
+export class UserDocumentsPageComponent {
   constructor() {
     this.eventListenerService = eventListenerService;
     this.documentPreferencesService = documentPreferencesService;
@@ -18,10 +18,10 @@ export class UserDocumentsPageComponent{
         this.routingService.setRoute("/editor");
         this.documentPreferencesService.documentId = generateDocumentId();
       },
-    }
+    },
   ];
 
-  _pushEvents(){
+  _pushEvents() {
     this.eventListenerService.events.push(...this.events);
   }
 
@@ -30,10 +30,23 @@ export class UserDocumentsPageComponent{
 
     return `
         <div class="container">
-            <h1> My Documents </h1>
-            <div id="userDocumentsContainer">
-              <button id="new-document-btn">Create New Document + </button>
+          <div class="userDocumentsContainer">
+            <h1>My Documents</h1>
+
+            <div class="documentsContainer">
+
+              <div id="new-document-btn" class="card">
+                <i class="card-logo bi bi-plus"></i>
+              </div>
+
+              <!--div id="new-document-btn" class="card">
+                <i class="bi bi-file-earmark card-logo"></i>
+                <div class="card-title">My Document Name</div>
+              </div-->
+
             </div>
+            
+          </div>
         </div>
         `;
   }
--- frontend/services/api/documentServiceApi.js
+++ frontend/services/api/documentServiceApi.js
@@ -2,50 +2,57 @@ import { documentPreferencesService } from "../documentPreferencesService.js";
 import { userAuthenticationService } from "./userAuthenticationService.js";
 
 class DocumentServiceApi {
-
   constructor() {
     this.documentPreferencesService = documentPreferencesService;
     this.userAuthenticationService = userAuthenticationService;
   }
 
-  retrieveAllDocuments() {
-
+  async retrieveAllDocuments() {
+    await fetch("/allUserDocuments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: authenticatedUser.username,
+        token: authenticatedUser.jwtToken
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log("Success:", data))
+      .catch((error) => console.error("Error:", error));
   }
 
-  retrieveDocumentById(id) {
-
-  }
+  retrieveDocumentById(id) {}
 
   saveDocument() {
     const documentId = this.documentPreferencesService.documentId;
     const documentName = this.documentPreferencesService.getDocumentTitle();
-    const documentContent = this.documentPreferencesService.getDocumentContent();
+    const documentContent =
+      this.documentPreferencesService.getDocumentContent();
     const documentPreferences = this.documentPreferencesService.preferences;
     const authenticatedUser = this.userAuthenticationService.getUser();
 
-    fetch('/save', {
-        method: 'POST', 
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            username: authenticatedUser.username,
-            token: authenticatedUser.jwtToken,
-            documentId: documentId,
-            documentName: documentName,
-            documentContent: documentContent,
-            documentPreferences: documentPreferences
-        })
+    fetch("/save", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: authenticatedUser.username,
+        token: authenticatedUser.jwtToken,
+        documentId: documentId,
+        documentName: documentName,
+        documentContent: documentContent,
+        documentPreferences: documentPreferences,
+      }),
     })
-    .then(response => response.json())
-    .then(data => console.log('Success:', data))
-    .catch(error => console.error('Error:', error));
+      .then((response) => response.json())
+      .then((data) => console.log("Success:", data))
+      .catch((error) => console.error("Error:", error));
   }
 
-  deleteDocumentById(id) {
-
-  }
-  
+  deleteDocumentById(id) {}
 }
 
 export const documentServiceApi = new DocumentServiceApi();
