33e71ac9ecd34a615af6a4644b9f1b6bb2999410
Nicholas Jones <njonesgrad@gmail.com>
2025-04-05 12:27:18 +0200

    Add Route to Retrieve User Document by Id

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -113,6 +113,28 @@ router.post("/allUserDocuments", async (req, res) => {
               message: "Retrieved user documents.",
             });
         } else {
+router.post("/userDocument", async (req, res) => {
+  const data = req.body;
+  try {
+    const decoded = jwt.verify(data.token, process.env.JWT_SECRET);
+    const username = req.body.username;
+    const documentId = req.body.documentId;
+
+    if (decoded) {
+      try {
+        const user = await User.findOne({ username: username }).populate(
+          "documents"
+        );
+
+        const matchedDocument = user.documents.filter(
+          (document) => document.documentId === documentId
+        );
+
+        if (matchedDocument) {
+          res.status(200).json({
+            document: matchedDocument,
+            message: "Retrieved user document.",
+          });
         }
       } catch (error) {
         res.status(400).json({ message: "Cannot locate user documents." });
