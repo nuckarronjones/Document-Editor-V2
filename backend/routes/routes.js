df117c53ef9083d72eb6a8ac3d065917232ccfa9
Nicholas Jones <njonesgrad@gmail.com>
2025-04-05 15:55:42 +0200

    Refine Save Api
    If the user saves a document, we need to verify if the document already exists. If so, simply update the existing document, else simply just create a new document.

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -17,28 +17,56 @@ router.get("*", (req, res) => {
 
 router.post("/save", async (req, res) => {
   const data = req.body;
+  const username = req.body.username;
+  const documentId = req.body.documentId;
 
   try {
     const decoded = jwt.verify(data.token, process.env.JWT_SECRET);
 
     if (decoded) {
       try {
-        await User.findOneAndUpdate(
-          { username: data.username },
-          {
-            $push: {
-              documents: {
-                documentId: data.documentId,
-                documentName: data.documentName,
-                documentContent: data.documentContent,
-                documentPreferences: data.documentPreferences,
+        const user = await User.findOne({ username: username }).populate(
+          "documents"
+        );
+
+        const matchedDocument = user.documents.findIndex(
+          (document) => document.documentId === documentId
+        );
+
+        if(matchedDocument !== -1){
+          await User.findOneAndUpdate(
+            { 
+              username: data.username, 
+              "documents.documentId": data.documentId 
+            },
+            {
+              $set: {
+                "documents.$.documentName": data.documentName, 
+                "documents.$.documentContent": data.documentContent,
+                "documents.$.documentPreferences": data.documentPreferences,
               },
             },
-          },
-          { new: true }
-        );
+            { new: true }
+          );
+        }else{
+          await User.findOneAndUpdate(
+            { username: data.username },
+            {
+              $push: {
+                documents: {
+                  documentId: data.documentId,
+                  documentName: data.documentName,
+                  documentContent: data.documentContent,
+                  documentPreferences: data.documentPreferences,
+                },
+              },
+            },
+            { new: true }
+          );
+        }
+
       } catch (error) {
-        console.error("Error adding posts:", error);
+        console.error("Error saving document:", error);
       }
     }
   } catch {
